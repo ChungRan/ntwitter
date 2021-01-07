582578445b9c7c8b758c95ecbdad1e6ba07e0b61
청 <riplac0314@naver.com>
2021-01-07 15:18:28 +0900

    #2.1

--- src/components/App.js
+++ src/components/App.js
@@ -3,8 +3,7 @@ import AppRouter from "components/Router";
 import {authService} from "fbase";
 
 function App() {
-  console.log(authService.currentUser);
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [isLoggedIn, setIsLoggedIn] = useState(authService.currentUser);
   return (
     <>
       <AppRouter isLoggedIn={isLoggedIn}></AppRouter>
@@ -14,3 +13,5 @@ function App() {
 }
 
 export default App;
+
+
--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,5 +1,46 @@
 // export default () => <span>Auth</span>
 
+import { useState } from "react";
+
 // 자동으로 import 할 수 있게 하려면 다음과 같이 쓴다
-const Auth = () => <span>Auth</span>
+const Auth = () => {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const onChange = (event) => {
+        const {target : {name, value}} = event;
+        if(name === "email"){
+            setEmail(value)
+        } else if (name ==="password"){
+            setPassword(value);
+        }
+    };
+    const onSubmit = (event) => {
+        event.preventDefault();
+    }
+    return (
+        <div>
+            <form onSubmit={onSubmit}>
+                <input 
+                name="email"
+                type="text"
+                placeholder="Email"
+                required value={email}
+                onChange={onChange}
+                />
+                <input 
+                name="password"
+                type="password"
+                placeholder="Password"
+                required value={password} 
+                onChange={onChange}
+                />
+                <input type="submit" value="Log In"></input>
+            </form>
+            <div>
+                <button>Continue with Google</button>
+                <button>Continue with Github</button>
+            </div>
+        </div>
+    )
+}
 export default Auth;
