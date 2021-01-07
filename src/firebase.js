80393b427b6bc621511a89531adda284b8cc849a
청 <riplac0314@naver.com>
2021-01-07 10:52:16 +0900

    2021 - 01 - 07

---
+++ src/firebase.js
@@ -0,0 +1,13 @@
+//import * as firebase from "firebase/app";
+import firebase from "firebase/app";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCtUiuR15LYLw78-WDQKSCE0Sz6rd-7pGo",
+    authDomain: "ntwitter-ea748.firebaseapp.com",
+    projectId: "ntwitter-ea748",
+    storageBucket: "ntwitter-ea748.appspot.com",
+    messagingSenderId: "882375645819",
+    appId: "1:882375645819:web:8e918eda327e07b969a0de"
+  };
+
+  export default firebase.initializeApp(firebaseConfig);
