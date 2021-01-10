86404f613a3d247f6ca87b615ceffac6fb89fb2d
청 <riplac0314@naver.com>
2021-01-10 13:49:09 +0900

    #2.4

--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,11 +1,14 @@
 // export default () => <span>Auth</span>
 
+import { authService } from "fbase";
 import { useState } from "react";
 
 // 자동으로 import 할 수 있게 하려면 다음과 같이 쓴다
 const Auth = () => {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
+    const [newAccount, setNewAccount] = useState(true);
+    const [error, setError] = useState("");
     const onChange = (event) => {
         const {target : {name, value}} = event;
         if(name === "email"){
@@ -14,15 +17,32 @@ const Auth = () => {
             setPassword(value);
         }
     };
-    const onSubmit = (event) => {
+    
+    const onSubmit = async(event) => {
         event.preventDefault();
-    }
+        try{
+            if(newAccount){
+                const data = await authService.createUserWithEmailAndPassword(
+                    email, password
+                )
+            } else {
+                const data = await authService.signInAndRetrieveDataWithCredential(
+                    email, password);
+            }
+            console.log(data);
+        } catch(error) {
+            setError(error.message);
+        }
+    };
+    const toggleAccount = () => setNewAccount((prev) => !prev);
     return (
         <div>
+            {/* onsubmit : form 태그 안에서 form전송을 하기 전에 입력된 데이터의 유효성을 체크하기 위해 사용하는 이벤트. */}
             <form onSubmit={onSubmit}>
+                {/* input tag https://developer.mozilla.org/ko/docs/Web/HTML/Element/Input */}
                 <input 
                 name="email"
-                type="text"
+                type="email"
                 placeholder="Email"
                 required value={email}
                 onChange={onChange}
@@ -34,8 +54,10 @@ const Auth = () => {
                 required value={password} 
                 onChange={onChange}
                 />
-                <input type="submit" value="Log In"></input>
+                <input type="submit" value={newAccount ? "Create Account" : "Log In"}></input>
+                {error};
             </form>
+            <span onClick={toggleAccount}>{newAccount ? "Sign in" : "Create Account"}</span>
             <div>
                 <button>Continue with Google</button>
                 <button>Continue with Github</button>
